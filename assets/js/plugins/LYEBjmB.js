2c0e9b1c5be874c3458e32437f5cd866a51970d6
Adhik Bagul <baguladhik@gmail.com>
2023-02-17 23:50:38 +0530

    Home pages design updated

---
+++ assets/js/plugins/LYEBjmB.js
@@ -0,0 +1,54 @@
+const wrapper = document.createElement('div');
+wrapper.innerHTML = `
+<div class="social-panel-container">
+	<div class="social-panel">
+		<p>Created with <i class="fa fa-heart"></i> by
+			<a target="_blank" href="https://florin-pop.com">Florin Pop</a></p>
+		<button class="close-btn"><i class="fas fa-times"></i></button>
+		<h4>Get in touch on</h4>
+		<ul>
+			<li>
+				<a href="https://twitter.com/florinpop1705" target="_blank">
+					<i class="fab fa-twitter"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://www.youtube.com/channel/UCeU-1X402kT-JlLdAitxSMA/" target="_blank">
+					<i class="fab fa-youtube"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://linkedin.com/in/florinpop17" target="_blank">
+					<i class="fab fa-linkedin"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://facebook.com/florinpop17" target="_blank">
+					<i class="fab fa-facebook"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://instagram.com/florinpop17" target="_blank">
+					<i class="fab fa-instagram"></i>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<button class="floating-btn">Get in Touch</button>
+`;
+
+document.body.appendChild(wrapper);
+
+// adding events listeners
+const social_panel_container = document.querySelector('.social-panel-container');
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
